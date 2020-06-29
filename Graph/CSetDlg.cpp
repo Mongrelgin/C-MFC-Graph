c971c9fbc43df902749f01b2d68aecb39e0327cc
Mongrelgin <43989334+Mongrelgin@users.noreply.github.com>
2020-06-29 19:17:55 +0300

    Add files via upload

---
+++ Graph/CSetDlg.cpp
@@ -0,0 +1,43 @@
+﻿// CSetDlg.cpp: файл реализации
+//
+
+#include "pch.h"
+#include "Graph.h"
+#include "CSetDlg.h"
+#include "afxdialogex.h"
+
+
+// Диалоговое окно CSetDlg
+
+IMPLEMENT_DYNAMIC(CSetDlg, CDialogEx)
+
+CSetDlg::CSetDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IIDD_CUSTOM_DIALOG, pParent)
+	, m_RedRadio(0)
+{
+
+}
+
+CSetDlg::~CSetDlg()
+{
+}
+
+void CSetDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Radio(pDX, IDC_RED_RADIO, m_RedRadio);
+}
+
+
+BEGIN_MESSAGE_MAP(CSetDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_RED_RADIO, &CSetDlg::OnClickedRedRadio)
+END_MESSAGE_MAP()
+
+
+// Обработчики сообщений CSetDlg
+
+
+void CSetDlg::OnClickedRedRadio()
+{
+	UpdateData(TRUE);
+}
---
+++ Graph/CSetDlg.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+// Диалоговое окно CSetDlg
+
+class CSetDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CSetDlg)
+
+public:
+	CSetDlg(CWnd* pParent = nullptr);   // стандартный конструктор
+	virtual ~CSetDlg();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IIDD_CUSTOM_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_RedRadio;
+	afx_msg void OnClickedRedRadio();
+};
